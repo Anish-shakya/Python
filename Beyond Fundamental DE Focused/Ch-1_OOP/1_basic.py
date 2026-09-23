8d5251e056ff2368baecb8f7e21ddd7425fb977c
anishshakya10 <anish.shakya@omc.com>
2026-09-23 14:14:29 +0545

    New branch commit

---
+++ Beyond Fundamental DE Focused/Ch-1_OOP/1_basic.py
@@ -0,0 +1,18 @@
+
+class MyClass:
+    # Class variables
+    var1 = "Anish"
+    var2 = "Shakya"
+
+    def func1(self):
+        print("Hello World")
+
+    def func2(self):
+        print("Hello Globe")
+
+
+# Create Python Object
+obj = MyClass()
+obj2 = MyClass()
+obj.func1()
+obj2.func2()
---
+++ Beyond Fundamental DE Focused/Ch-1_OOP/4_DataExt_Class.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+class DataExt:
+
+    def __init__(self,file_path:str):
+        self.file_path=file_path
+
+    def fetch_text(self,separator:str):
+        #write your custom logic
+        df=pd.read_csv(self.file_path,sep=separator)
+        print(df.head())
+
+    def fetch_json(self):
+        df = pd.read_json(self.file_path)
+        print(df.head())
+
+    def fetch_parquet(self):
+        df =pd.read_parquet(self.file_path)
+        print(df.head())
+
+
+obj = DataExt("Files/orders.json")
+##obj.fetch_text(',')
+##obj.fetch_text("\t")
+
+obj.fetch_json()
+
